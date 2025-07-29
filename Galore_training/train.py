fbf11da7b243befbf25c190b9dbd57e0f26e476a
Darren <dattafuu@gmail.com>
2025-07-30 02:57:43 +0800

    Modified for GLUE

--- Galore_training/train.py
+++ Galore_training/train.py
@@ -1,31 +1,25 @@
+import torch
 import torch.nn as nn
+import torch.optim as optim
 from transformers import Trainer, TrainingArguments
-from config import BATCH_SIZE, EPOCHS, LEARNING_RATE, OPTIM, OPTIM_TARGET_MODULES, PROJ_TYPE, RANK, SCALE, SEED, UPDATE_PROJ_GAP
+from config import Config_Args
+from config import OPTIM, OPTIM_TARGET_MODULES, PROJ_TYPE, RANK, SCALE, SEED, UPDATE_PROJ_GAP
+from metrics import SystemMetricsCallback
 
-# class CustomTrainer(Trainer):
-#     def __init__(self, criterion=None, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.criterion = criterion if criterion is not None else nn.CrossEntropyLoss()
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    # Convert NumPy arrays to PyTorch Tensors
+    predictions = torch.tensor(predictions)
+    labels = torch.tensor(labels)
+    loss = nn.CrossEntropyLoss()(predictions, labels)
+    return {"eval_loss": loss.item()}
 
-#     def compute_loss(self, model, inputs, return_outputs=False, *args, **kwargs):
-#         """
-#         Criterion -> loss function
-#         """
-#         # Extract inputs and labels
-#         outputs = model(**inputs)
-#         logits = outputs.logits
-#         labels = inputs["labels"]
-
-#         loss = self.criterion(logits, labels)
-
-#         return (loss, outputs) if return_outputs else loss
-
-def setup_trainer(model, train_dataset, test_dataset):
-    """Set up the Hugging Face Trainer with GaLore optimizer."""
+def setup_trainer(model, tokenizer, train_dataset, test_dataset):
     training_args = TrainingArguments(
-        num_train_epochs=EPOCHS,
-        per_device_train_batch_size=BATCH_SIZE,
-        learning_rate=LEARNING_RATE,
+        output_dir="./output",
+        num_train_epochs=Config_Args.args.epochs,
+        per_device_train_batch_size=Config_Args.args.train_batch_size,
+        learning_rate=Config_Args.args.lr,
         seed=SEED,
         optim=OPTIM,
         optim_args = f"rank={RANK}, update_proj_gap={UPDATE_PROJ_GAP}, scale={SCALE}, prof_type={PROJ_TYPE}",  # Pass GaLore hyperparameters
@@ -34,7 +28,8 @@ def setup_trainer(model, train_dataset, test_dataset):
         # These are passed to the optimizer via TrainingArguments
         # rank, update_proj_gap, scale, proj_type are handled internally by galore-torch
 
-        logging_dir="output/training.log",
+        logging_dir="./logs",
+        report_to="tensorboard",
         logging_steps=100,  # Log every 100 steps
         logging_strategy="steps",
         save_strategy="no", # Set to "epoch" to save model after every epoch
@@ -47,7 +42,8 @@ def setup_trainer(model, train_dataset, test_dataset):
         # criterion=nn.CrossEntropyLoss(),
         train_dataset=train_dataset,
         eval_dataset=test_dataset,
-        compute_metrics=nn.CrossEntropyLoss()
+        compute_metrics=compute_metrics,
+        callbacks=[SystemMetricsCallback(log_dir=training_args.logging_dir, model=model, tokenizer=tokenizer)]
     )
 
     return trainer
