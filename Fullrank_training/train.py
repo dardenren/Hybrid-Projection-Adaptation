ef78e318f4a178c44175034f99fe842fb32123fc
Darren <dattafuu@gmail.com>
2025-08-03 03:29:59 +0800

    Added learning rate scheduler

--- Fullrank_training/train.py
+++ Fullrank_training/train.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from transformers import Trainer, TrainingArguments
-from config import Config_Args
+from config import Config_Args, DEVICE
 from metrics import SystemMetricsCallback
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
     # Convert NumPy arrays to PyTorch Tensors
-    predictions = torch.tensor(predictions)
-    labels = torch.tensor(labels)
+    predictions = torch.tensor(predictions, device=DEVICE)
+    labels = torch.tensor(labels, device=DEVICE)
     # Compute loss
     loss = nn.CrossEntropyLoss()(predictions, labels)
     return {"eval_loss": loss.item()}
@@ -32,6 +32,8 @@ def setup_trainer(model, tokenizer, train_dataset, test_dataset):
         num_train_epochs=Config_Args.args.epochs,
         per_device_train_batch_size=Config_Args.args.train_batch_size,
         learning_rate=Config_Args.args.lr,
+        warmup_ratio = 0.1,
+        lr_scheduler_type="linear",
         seed=Config_Args.args.seed,
         # logging_dir="./logs",
         logging_dir=logging_dir_string,
@@ -47,7 +49,7 @@ def setup_trainer(model, tokenizer, train_dataset, test_dataset):
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=test_dataset,
-        compute_metrics=compute_metrics,
+        compute_metrics=nn.CrossEntropyLoss(),
         optimizers=(optimizer, None),
         callbacks=[SystemMetricsCallback(log_dir=training_args.logging_dir, model=model, tokenizer=tokenizer)]
     )
